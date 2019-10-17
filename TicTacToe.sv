7404f0571013bfaab6f0b7682e003b8f5e30258e
edmobe <31488944+edmobe@users.noreply.github.com>
2019-10-17 03:32:58 -0600

    Fix sprites' orientation

--- TicTacToe.sv
+++ TicTacToe.sv
@@ -17,16 +17,16 @@ module TicTacToe(
 	logic [1:0] block00;
 	logic [1:0] block01;
 	logic [1:0] block02;
-	logic [1:0] block10;
+	//logic [1:0] block10;
 	logic [1:0] block11;
 	logic [1:0] block12;
 	logic [1:0] block20;
 	logic [1:0] block21;
 	logic [1:0] block22;
-	logic [3:0] selected;
+	//logic [3:0] selected;
 	
 
-	// PLEASE MODIFY THIS VALUES BASED ON THE GAME LOGIC :)
+	// ****************** PLEASE MODIFY THIS VALUES BASED ON THE GAME LOGIC :) *******************
 	// Sprite selectors: 00 or 11 = no sprite, 01 = sprite 1, 10 = sprite 2
 	// Block 00
 	assign block00 = 2'b11;
@@ -35,7 +35,7 @@ module TicTacToe(
 	// Block 02
 	assign block02 = 2'b10;
 	// Block 10
-	assign block10 = 2'b00;
+	//assign block10 = 2'b00;
 	// Block 11
 	assign block11 = 2'b10;
 	// Block 12
@@ -48,20 +48,21 @@ module TicTacToe(
 	assign block22 = 2'b00;
 	
 	// Selected (indicates the selected block)
-	assign selected = 4'b00;
+	//assign selected = 4'b0000;
+	// *******************************************************************************************
 
 	video_controller VGA (
 	CLOCK_50,
 	block00,
 	block01,
 	block02,
-	block10,
+	SW[5:4],
 	block11,
 	block12,
 	block20,
 	block21,
 	block22,
-	selected,
+	SW[3:0],
 	VGA_HS,
 	VGA_VS,
 	VGA_R,
@@ -72,32 +73,3 @@ module TicTacToe(
 	VGA_BLANK_N);
 	
 endmodule
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-module TicTacToe(
-	input logic CLK,
-	input logic[9:0] x,
-	input logic[9:0] y,
-	output logic[7:0] VGA_R,
-	output logic[7:0] VGA_G,
-	output logic[7:0] VGA_B
-	);
-
-	sprite_controller ctrl(CLK, x, y, VGA_R, VGA_G, VGA_B);
-
-endmodule
-*/	
